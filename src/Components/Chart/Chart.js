2b0801361d00cc34003c2a26c28be2fa44ba5c93
Michael Yaacoub <michael.yaacoub7@gmail.com>
2021-06-15 04:18:30 -0700

    add chartBar

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -3,7 +3,18 @@ import React from 'react';
 
 import ChartBar from './ChartBar';
 import './Chart.css';
-
+import styled from 'styled-components'
+const Chart = styled.div`
+{
+  padding: 1rem;
+  border-radius: 12px;
+  background-color: #7C83FD;
+  text-align: center;
+  display: flex;
+  justify-content: space-around;
+  height: 10rem;
+  box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
+}`;
 const Chart = (props) => {
   const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
   const totalMaximum = Math.max(...dataPointValues);
--- src/Components/Chart/ChartBar.js
+++ src/Components/Chart/ChartBar.js
@@ -1,8 +1,38 @@
-
 import React from 'react';
 
-import './ChartBar.css';
+import styled from 'styled-components'
+const Bar = styled.div`
+{
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.chart-bar__inner {
+  height: 100%;
+  width: 100%;
+  border: 1px solid #b8c4ce;
+  border-radius: 12px;
+  background-color: #eff2f5;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
 
+.chart-bar__fill {
+  background-color: #0c1b2e;
+  width: 100%;
+  transition: all 0.3s ease-out;
+}
+
+.chart-bar__label {
+  font-weight: bold;
+  font-size: 0.5rem;
+  text-align: center;
+  color: #0c1b2e;
+}
+`;
 const ChartBar = (props) => {
   let barFillHeight = '0%';
 
@@ -11,7 +41,7 @@ const ChartBar = (props) => {
   }
 
   return (
-    <div className='chart-bar'>
+    <Bar>
       <div className='chart-bar__inner'>
         <div
           className='chart-bar__fill'
@@ -19,7 +49,7 @@ const ChartBar = (props) => {
         ></div>
       </div>
       <div className='chart-bar__label'>{props.label}</div>
-    </div>
+    </Bar>
   );
 };
 
